d3a94fe30fa425ac7ebe2177e27d71205d7a55f0
Aaron John Bitara <118180256+eyron29@users.noreply.github.com>
2025-02-26 20:01:02 +0800

    feat: home page

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <div class="nav-bar">
+        <ul>
+            <!-- <div class="left-link">
+                <h2>Byte Metrics</h2>
+            </div> -->
+            <div class="right-links">
+            <li><a href="">Home</a></li>
+            <li><a href="">Services</a></li>
+            <li><a href="">About</a></li>
+            <li><a href="">Projects</a></li>
+            <li><a href="">Contact</a></li>
+            <li><a href="">Login</a></li>
+            <!-- <div class="profile-logo">
+                <img src="images/profile_logo.png"alt="Analytics">
+            </div> -->
+        </ul>
+        <div class="profile-logo">
+        <img src="images/profile_logo.png"alt="Analytics">
+        </div>
+    </div>
+
+    <div class="content-container">
+        <div class="content-txt">
+            <h1 id="cnt-title">Turning Data Into Decisions That Matter</h1>
+            <p id="cnt-descrip">Make informed decisions with confidence. Byte Metrics provides deep insights that turn your business data into opportunities for success.</p>
+            <button id="inquire-btn">Inquire Now</button>
+        </div>
+        <div class="content-img">
+            <img src="images/analytics_bg.png"alt="Analytics">
+        </div>
+    </div>
+    
+</body>
+</html>
