8aa8832eb7bf566ccc546ac32573c8fb98c6708a
Aaron John Bitara <118180256+eyron29@users.noreply.github.com>
2025-03-17 16:09:59 +0800

    feat: signup page

---
+++ signup.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="styles1.css">
+</head>
+<body>
+    <div class="form-container">
+        <div class="container">
+            <form action="">
+                <h1 id="title">Create Account</h1>
+                <div id="divider"></div>
+                <input type="text" name="" placeholder="Username" class="username">
+                <input type="email" name="" placeholder="Email Address" class="emailAdd">
+                <input type="password" name="" placeholder="Password" class="password">
+                <input type="password" name="" placeholder="Confirm Password" class="password">
+
+                <input type="submit" value="Signup">
+                <label for="">Already have an account? <span id="sign">Sign In Now</span></label>
+            </form>
+        </div>
+    </div>
+    
+</body>
+</html>
